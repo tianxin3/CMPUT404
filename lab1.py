0f77cbc5e7f7a256c71b34d654c8a8b985015c79
Tianxin Ma <tianxin3@cs.ualberta.ca>
2020-01-10 09:54:17 -0700

    Update: download itself and print its content

--- lab1.py
+++ lab1.py
@@ -6,3 +6,7 @@ google = requests.get("http://google.com")
 print(google)
 
 
+# raw url from github
+var = requests.get("https://raw.githubusercontent.com/tianxin3/CMPUT404/master/lab1.py")
+
+print(var.content)
